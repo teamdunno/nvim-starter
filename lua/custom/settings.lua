1276fb8ba7caca9257aa83ce0e80f2290571fa55
teesh3rt <ilaylevy611@gmail.com>
2025-04-24 11:43:32 +0300

    init: ready, set, go!

---
+++ lua/custom/settings.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+M.ui = {
+    theme = "gruvbox",
+}
+
+M.extras = {
+    "cosmetic.gruvbox",
+    "lang.lua",
+    "lang.markdown",
+    "editor.searchbox",
+    "cosmetic.noice",
+}
+
+return M
